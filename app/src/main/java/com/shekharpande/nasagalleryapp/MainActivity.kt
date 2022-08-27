19309d2e0b2bffdf7804747732df20f3f6a2209e
shekhar <spande6@gmail.com>
2022-08-27 17:56:06 +0530

    - Created project. - Added all dependencies.

---
+++ app/src/main/java/com/shekharpande/nasagalleryapp/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.shekharpande.nasagalleryapp
+
+import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.gif.GifDrawable
+import com.bumptech.glide.request.target.Target
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
